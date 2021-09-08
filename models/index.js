65a5d766a05b92e085ea35ec097eba9a03befb6d
Janet Iqal <j.iqal35@gmail.com>
2021-09-08 13:17:10 -0500

    columns created for tables,defined relationships bw tables

--- models/index.js
+++ models/index.js
@@ -5,12 +5,42 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category,{
+  foreignKey:'category_id',
+  onDelete: "SET NULL",
+  onUpdate: "CASCADE"
+})
 
 // Categories have many Products
+Category.hasMany(Product,{
+  foreignKey:'category_id',
+  onDelete: "SET NULL",
+  onUpdate: "CASCADE"
+})
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongToMany Tags (through ProductTag, aka the junction model)
+Product.belongsToMany(Tag,{
+  through:{
+    model: ProductTag,
+    unique: false,
+    // foreignKey: 'product_tag',
+    onDelete: "SET NULL",
+    onUpdate: "CASCADE"
+  },//alias
+    as: 'products_tagged'
+})
 
-// Tags belongToMany Products (through ProductTag)
+// Tags belongToMany Products (through ProductTag aka the junction model)
+Tag.belongsToMany(Product, {
+ through:{
+   model: ProductTag,
+   unique: false,
+  //  foreignKey: 'tag_id',
+   onDelete: "SET NULL",
+   onUpdate: "CASCADE"
+},//alias
+  as: 'tags_of_products'
+})
 
 module.exports = {
   Product,
